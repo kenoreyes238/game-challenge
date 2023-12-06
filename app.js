9f4ecf2938df1298e68d91271de6a4e7894a6894
Keno Reyes <keno.reyes.cohort238@gmail.com>
2023-12-06 08:45:37 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+function Player(name) {
+    this.name = name;
+    this.lvl = 1;
+    this.points = 0;
+}
+
+Player.prototype.gainXp = function(xp) {
+    this.points += xp;
+
+    if(this.points >= 10) {
+        this.lvl++;
+        this.points -= 10;
+    }
+
+    console.log(this.describe());
+}
+
+Player.prototype.describe = function() {
+    return `${this.name} is level ${this.lvl} with ${this.points}`;
+}
+
+let player1 = new Player('Bob');
+let player2 = new Player('Alice');
+
+player1.gainXp(5);
+player2.gainXp(7);
+player1.gainXp(3);
+player2.gainXp(2);
+player1.gainXp(8);
+player2.gainXp(4);
+
+// console.log(player1.describe());
+// console.log(player2.describe());
